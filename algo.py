43de085cb8df1d69e55269fc1a879a8b93dc943f
mschandana16 <chandanasuresh2004@gmail.com>
2023-10-25 12:03:28 +0530

    Updated the code

---
+++ algo.py
@@ -0,0 +1,32 @@
+# This is the code for level 3 of the hunt
+# Input: X number of posts on her_281023's insta page = 4
+# Output: 1211
+
+
+def algo(n):
+    if n == 1:
+        return "1"
+    if n == 2:
+        return "11"
+
+    s = "11"
+    for i in range(3, n + 1):
+        s += "$"
+        l = len(s)
+        count = 1
+        tmp = ""
+        for j in range(1, l):
+            if s[j] != s[j - 1]:
+                tmp += str(count)
+                tmp += s[j - 1]
+                count = 1
+            else:
+                count += 1
+        s = tmp
+    return s
+
+
+# Initialize X with the number you got from level_2
+# X =
+X = (4,)  # this is expected of them
+print(algo(X))
